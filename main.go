bccf3664d020c0dd826763334d9a20baa386fc8d
WatchJani <jankokondic84@gmail.com>
2024-06-21 17:24:23 +0200

    refactoring

--- main.go
+++ main.go
@@ -1,130 +1,13 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"hash/crc32"
 	"log"
-	"os"
-	"sort"
+	c "root/consistent_hashing"
 )
 
-type Server struct {
-	Addr string `json:"ip"`
-	Name string `json:"name"`
-}
-
-type VirtualServer struct {
-	Name      string
-	HashValue uint32
-	*Server
-}
-
-type Configuration struct {
-	VirtualNodeServer int      `json:"nodeNumber"`
-	Servers           []Server `json:"servers"`
-}
-
-func NewConfiguration() *Configuration {
-	return &Configuration{
-		Servers: make([]Server, 0),
-	}
-}
-
-type ConsistentHashing struct {
-	*Configuration
-	VirtualServers []VirtualServer
-	Namer          int
-}
-
-func (c *ConsistentHashing) NewVirtualServer(server *Server) VirtualServer {
-	c.Namer = (c.Namer + 1) % c.VirtualNodeServer
-	nodeID := c.Namer
-	name := fmt.Sprintf("%s_%d", server.Name, nodeID)
-
-	return VirtualServer{
-		Name:      name,
-		HashValue: crc32.ChecksumIEEE([]byte(name)),
-		Server:    server,
-	}
-}
-
-func NewConsistentHashing() *ConsistentHashing {
-	return &ConsistentHashing{
-		Configuration:  NewConfiguration(),
-		VirtualServers: make([]VirtualServer, 0),
-	}
-}
-
-// load configuration file and decode
-func (c *ConsistentHashing) LoadConfig(path string) error {
-	file, err := os.Open(path)
-	if err != nil {
-		return err
-	}
-	defer file.Close()
-
-	decoder := json.NewDecoder(file)
-	if err := decoder.Decode(c.Configuration); err != nil {
-		return err
-	}
-
-	return nil
-}
-
-// create all virtual server in ring
-func (c *ConsistentHashing) Create() {
-	for i := 0; i < len(c.Servers); i++ {
-		for j := 0; j < c.VirtualNodeServer; j++ {
-			c.VirtualServers = append(c.VirtualServers, c.NewVirtualServer(&c.Servers[i]))
-		}
-	}
-}
-
-func (c *ConsistentHashing) Sort() {
-	sort.Slice(c.VirtualServers, func(i, j int) bool {
-		return c.VirtualServers[i].HashValue < c.VirtualServers[j].HashValue
-	})
-}
-
-func (c *ConsistentHashing) Load(path string) error {
-	if err := c.LoadConfig(path); err != nil {
-		return err
-	}
-
-	c.Create() //create all virtual servers
-	c.Sort()   //sort all virtual servers
-
-	return nil
-}
-
-func Search(arr []VirtualServer, target uint32) int {
-	left, right := 0, len(arr)-1
-	result := len(arr)
-
-	for left <= right {
-		mid := left + (right-left)/2
-
-		if arr[mid].HashValue >= target {
-			result = mid
-			right = mid - 1
-		} else {
-			left = mid + 1
-		}
-	}
-
-	return result
-}
-
-func (c *ConsistentHashing) FindServer(key string) *Server {
-	newHash := crc32.ChecksumIEEE([]byte(key))
-
-	virtualServerIndex := Search(c.VirtualServers, newHash)
-	return c.VirtualServers[virtualServerIndex].Server
-}
-
 func main() {
-	ch := NewConsistentHashing()
+	ch := c.NewConsistentHashing()
 	if err := ch.Load("./config.json"); err != nil {
 		log.Println(err)
 	}
